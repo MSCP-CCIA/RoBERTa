15269aa57d755bcbde854bd84062e78e1db804be
Andresmahu <andres.m.hurtado2907@gmail.com>
2024-09-04 13:35:01 -0500

    V1.1

---
+++ training/tests/test_RandomForest.py
@@ -0,0 +1,42 @@
+from sklearn.ensemble import RandomForestClassifier
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+resultados = {}
+auc_lis = []
+pr_lis = []
+f1_lis = []
+
+X_train, X_test, y_train, y_test = train_test_split(df, df["Class"], test_size=0.2, stratify=df["Class"], random_state=42)
+X_train.groupby("Class")["Time"].count()/X_train.groupby("Class")["Time"].count().sum()
+
+for sub in tqdm(submuestras):
+    model = RandomForestClassifier()
+    model.fit(sub[x_cols], sub[y_cols])
+
+    # Predicción
+    y_scores = model.predict_proba(X_test[x_cols])[:, 1]
+    y_pred = model.predict(X_test[x_cols])
+
+    # Calcular la curva AUC-ROC
+    fpr, tpr, _ = roc_curve(y_test, y_scores)
+    roc_auc = auc(fpr, tpr)
+
+    # Calcular la curva AUC-PR
+    precision, recall, _ = precision_recall_curve(y_test, y_scores)
+    pr_auc = auc(recall, precision)
+
+    # Realizar predicciones y calcular F1 Score
+    f1 = f1_score(y_test, y_pred)
+
+    auc_lis.append(roc_auc)
+    pr_lis.append(pr_auc)
+    f1_lis.append(f1)
+
+roc_auc = round(np.mean(auc_lis)*100,1)
+pr_auc = round(np.mean(pr_lis)*100,1)
+f1 = round(np.mean(f1_lis)*100,1)
+
+
+resultados["BosqueAleatorio"]=[roc_auc, pr_auc, f1]
+#%% md
+###**📈Regresión logistica📉**
