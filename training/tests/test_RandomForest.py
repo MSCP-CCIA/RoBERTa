c6ca2f55648e748a42d2238d8a3231dda48849fe
Khaelf0ns <mhatt.fons@gmail.com>
2024-09-10 20:04:05 -0500

    Updated RandomForest

--- training/tests/test_RandomForest.py
+++ training/tests/test_RandomForest.py
@@ -5,40 +5,53 @@ import numpy as np
 from tqdm import tqdm
 from training.tests.undersampling import UnderSampling
 import warnings
+from hydra import initialize, compose
+from omegaconf import OmegaConf
 
 warnings.filterwarnings("ignore")
 
-submuestras = UnderSampling()
-auc_lis = []
-pr_lis = []
-f1_lis = []
-X_test = pd.read_csv("data/X_test.csv")
-y_test = pd.read_csv("data/y_test.csv")
 
-for sub in tqdm(submuestras):
-    model = RandomForestClassifier()
-    model.fit(sub[0], sub[1])
+def main(cfg):
+    submuestras = UnderSampling()
+    auc_lis = []
+    pr_lis = []
+    f1_lis = []
 
-    # Predicción
-    y_scores = model.predict_proba(X_test)[:, 1]
-    y_pred = model.predict(X_test)
+    # Leer datos desde las rutas configuradas
+    X_test = pd.read_csv(cfg.data.x_test_path)
+    y_test = pd.read_csv(cfg.data.y_test_path)
 
-    # Calcular la curva AUC-ROC
-    fpr, tpr, _ = roc_curve(y_test, y_scores)
-    roc_auc = auc(fpr, tpr)
+    for sub in tqdm(submuestras):
+        model = RandomForestClassifier(n_estimators=cfg.model.n_estimators, max_depth=cfg.model.max_depth)
+        model.fit(sub[0], sub[1])
 
-    # Calcular la curva AUC-PR
-    precision, recall, _ = precision_recall_curve(y_test, y_scores)
-    pr_auc = auc(recall, precision)
+        # Predicción
+        y_scores = model.predict_proba(X_test)[:, 1]
+        y_pred = model.predict(X_test)
 
-    # Realizar predicciones y calcular F1 Score
-    f1 = f1_score(y_test, y_pred)
+        # Calcular la curva AUC-ROC
+        fpr, tpr, _ = roc_curve(y_test, y_scores)
+        roc_auc = auc(fpr, tpr)
 
-    auc_lis.append(roc_auc)
-    pr_lis.append(pr_auc)
-    f1_lis.append(f1)
+        # Calcular la curva AUC-PR
+        precision, recall, _ = precision_recall_curve(y_test, y_scores)
+        pr_auc = auc(recall, precision)
 
-roc_auc = round(np.mean(auc_lis) * 100, 1)
-pr_auc = round(np.mean(pr_lis) * 100, 1)
-f1 = round(np.mean(f1_lis) * 100, 1)
-print(f"f1:{f1},pr_auc:{pr_auc},roc_auc:{roc_auc}")
+        # Realizar predicciones y calcular F1 Score
+        f1 = f1_score(y_test, y_pred)
+
+        auc_lis.append(roc_auc)
+        pr_lis.append(pr_auc)
+        f1_lis.append(f1)
+
+    roc_auc = round(np.mean(auc_lis) * 100, 1)
+    pr_auc = round(np.mean(pr_lis) * 100, 1)
+    f1 = round(np.mean(f1_lis) * 100, 1)
+    print(f"f1:{f1}, pr_auc:{pr_auc}, roc_auc:{roc_auc}")
+
+
+if __name__ == "__main__":
+    with initialize(config_path=".", config_name="config"):
+        cfg = compose(config_name="config")
+        OmegaConf.resolve(cfg)  # Resolves any interpolation and placeholders
+        main(cfg)
